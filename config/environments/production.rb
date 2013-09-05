cd4ad09ec1a54b7c75f88de19d24615fec881c67
Luke Robertson <luke.robertson@mac.com>
2013-09-05 11:45:03 +0100

    setting assets to compile in production if they're not precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ FinancialServicesApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.compile = true
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -65,7 +66,7 @@ FinancialServicesApp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-config.assets.initialize_on_precompile = false
+
 
 
   CarrierWave.configure do |config|
